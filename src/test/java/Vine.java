096dd7558d9318849b0151f705dfa8069476d720
Baronas0927 <do.baublys@gmail.com>
2024-06-28 12:40:30 +0300

    skelbiult file

--- src/test/java/Vine.java
+++ src/test/java/Vine.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -8,29 +10,35 @@ import java.time.Duration;
 
 public class Vine {
     public static WebDriver driver;
+    public static WebDriverWait wait;
 
     @BeforeClass
     public void setup(){
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(25));
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        acceptAge();
+        acceptCookies();
     }
-    @Test
+
     public void acceptAge(){
         driver.get("https://vynoteka.lt/");
-        driver.findElement(By.xpath("//*[@id=\"app__inner\"]/div[2]/div/div/div/div/div[2]/div[3]/div/div[1]/button")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"app__inner\"]/div[2]/div/div/div/div/div[2]/div[3]/div/div[1]/button"))).click();
     }
-    @Test
+
     public void acceptCookies(){
         driver.findElement(By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
-        driver.findElement(By.id("omnisend-form-63ff1f31b40d6530aba59a6d-close-action")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.
+                xpath("/html/body/div[3]/div[3]/div/div[2]/div[2]/div[2]/button"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.
+                className("modal__close-button"))).click();
     }
     @Test
     public void search(){
         driver.findElement(By.className("form-control")).sendKeys("vynas");
-        driver.findElement(By.id("btn btn--sm secondary hidden-then-sticky")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.findElement(By.id("btn transparent modal__close-button")).click();
+        driver.findElement(By.xpath("//*[@id=\"app__header\"]/div[2]/div/div/div[3]/div/div/div/form/div[1]/div/input")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"app__header\"]/div[2]/div/div/div[3]/div/div/div/form/div[2]/div/div[2]/div/div/div/div[1]/div[1]/div/div/a")));
+       //                                         /html/body/div[2]/div[1]/header/div[2]/div/div/div[3]/div/div/div/form/div[1]/button
+        driver.findElement(By.xpath("/html/body/div[2]/div[1]/header/div[2]/div/div/div[3]/div/div/div/form/div[1]/button")).click();
     }
 }
---
+++ src/test/java/skelbiult.java
@@ -0,0 +1,58 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class skelbiult {
+    public static WebDriver driver;
+    public static WebDriver wait;
+
+    @BeforeClass
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        acceptCookies();
+    }
+    @Test
+    public void all() throws InterruptedException {
+        search("verpimo ratelis");
+        Thread.sleep(500);
+
+        int firstdataline1 = driver.findElements(By.className("first-dataline")).size();
+        int firstdataline2 = driver.findElements(By.className("first-dataline")).size();
+        int firstdataline3 = driver.findElements(By.className("first-dataline")).size();
+        int pages = firstdataline1 + firstdataline2 + firstdataline3;
+
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/div[2]/div[4]/div[1]/div[2]/div[3]/div[1]/a[1]")).click();
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/div[2]/div[4]/div[1]/div[2]/div[4]/div[1]/a[3]")).click();
+        System.out.println("rasta: " + pages);
+                ////html/body/div[3]/div[2]/div[2]/div[4]/div[1]/div[2]/div[4]/div[1]/a[3]
+    }
+
+    public void acceptCookies(){
+        driver.get("https://www.skelbiu.lt/");
+        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+    }
+    public void search(String searchWord){
+        driver.findElement(By.id("searchKeyword")).sendKeys(searchWord);
+        driver.findElement(By.id("searchButton")).click();
+//        driver.findElement(By.id("searchKeyword")).clear();
+    }
+//    public void stetoskopas(){
+//        driver.switchTo().newWindow(WindowType.TAB);
+//        driver.get("https://www.skelbiu.lt/");
+//        driver.findElement(By.id("searchKeyword")).sendKeys("stetoskopas");
+//    }
+//    public void butas(){
+//        driver.switchTo().newWindow(WindowType.TAB);
+//        driver.get("https://www.skelbiu.lt/");
+//        driver.findElement(By.id("searchKeyword")).sendKeys("butas");
+//    }
+//    public void asjedrhgfsjkhfbsef(){
+//        driver.switchTo().newWindow(WindowType.TAB);
+//        driver.get("https://www.skelbiu.lt/");
+//        driver.findElement(By.id("searchKeyword")).sendKeys("asjedrhgfsjkhfbsef");
+//    }
+}
+
